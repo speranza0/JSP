7521fbbaf7de5854a9274a3b969adce8a11d25c4
user <user@DESKTOP-P1K3VG9>
2022-05-19 18:01:55 +0900

    JSP 강의 19강 데이터베이스 회원가입

---
+++ jsp_ex19/WebContent/joinResult.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>Insert title here</title>
+</head>
+<body>
+	회원가입이 정상 처리 되었습니다.<br />
+	<a href="login.html">로그인</a>
+</body>
+</html>
---
+++ jsp_ex19/src/main/java/jsp_ex19/LoginOk.java
@@ -0,0 +1,95 @@
+package jsp_ex19;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginOk
+ */
+@WebServlet("/LoginOk")
+public class LoginOk extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	private Connection connection;
+	private Statement stmt;
+	private ResultSet resultSet;
+	
+	private String name, id, pw, phone1, phone2, phone3, gender;
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginOk() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		actionDo(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		actionDo(request, response);
+	}
+
+	private void actionDo(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		id = request.getParameter("id");
+		pw = request.getParameter("pw");
+		
+		String query = "select * from member where id = '" + id + "' and pw = '" + pw + "'";
+		
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/hello_world", "root", "1234");
+			stmt = connection.createStatement();
+			resultSet = stmt.executeQuery(query);
+			
+			while(resultSet.next()) {
+				name = resultSet.getString("name");
+				id = resultSet.getString("id");
+				pw = resultSet.getString("pw");
+				phone1 = resultSet.getString("phone1");
+				phone2 = resultSet.getString("phone2");
+				phone3 = resultSet.getString("phone3");
+				gender = resultSet.getString("gender");
+			}
+			
+			HttpSession httpSession = request.getSession();
+			httpSession.setAttribute("name", name);
+			httpSession.setAttribute("id", id);
+			httpSession.setAttribute("pw", pw);
+			
+			response.sendRedirect("loginResult.jsp");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(resultSet != null) resultSet.close();
+				if(stmt != null) stmt.close();
+				if(connection != null) connection.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
