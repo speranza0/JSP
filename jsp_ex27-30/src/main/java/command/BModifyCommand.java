81b422fe362d6dcf322bbb9cb07db8858251d0a5
user <user@DESKTOP-P1K3VG9>
2022-05-23 18:36:29 +0900

    JSP 강의 27~30강 mvc패턴을 이용한 게시판 만들기

---
+++ jsp_ex27-30/src/main/java/command/BModifyCommand.java
@@ -0,0 +1,21 @@
+package command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.BDao;
+
+public class BModifyCommand implements BCommand {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		String bId = request.getParameter("bId");
+		String bName = request.getParameter("bName");
+		String bTitle = request.getParameter("bTitle");
+		String bContent = request.getParameter("bContent");
+		
+		BDao dao = new BDao();
+		dao.modify(bId, bName, bTitle, bContent);
+	}
+}
---
+++ jsp_ex27-30/src/main/java/command/BWriteCommand.java
@@ -0,0 +1,14 @@
+package command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BWriteCommand implements BCommand {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
