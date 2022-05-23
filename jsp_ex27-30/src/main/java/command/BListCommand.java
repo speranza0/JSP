d4dfba86eeb1a9c5843b85d23fb1098b0a1c4a58
speranza0 <speranza0p@gmail.com>
2022-05-23 18:36:29 +0900

    JSP 강의 27~30강 mvc패턴을 이용한 게시판 만들기

---
+++ jsp_ex27-30/src/main/java/command/BListCommand.java
@@ -0,0 +1,21 @@
+package command;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.BDao;
+import dto.BDto;
+
+public class BListCommand implements BCommand {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		BDao dao = new BDao();
+		ArrayList<BDto> dtos = dao.list();
+		request.setAttribute("list", dtos);
+	}
+	
+}
